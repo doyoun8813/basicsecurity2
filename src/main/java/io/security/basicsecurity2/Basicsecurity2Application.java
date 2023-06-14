5aeffb80c715a0341f8b47773775cd3023f0d3e1
doyoun <doyoun8813@gmail.com>
2023-06-15 08:52:42 +0900

    create project

---
+++ src/main/java/io/security/basicsecurity2/Basicsecurity2Application.java
@@ -0,0 +1,12 @@
+package io.security.basicsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basicsecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Basicsecurity2Application.class, args);
+    }
+}
