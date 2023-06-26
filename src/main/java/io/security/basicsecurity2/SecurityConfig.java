76ed6912ad2448ce8cb1ca850054f18406e00840
doyoun <doyoun8813@gmail.com>
2023-06-27 00:55:03 +0900

    다중 설정 우선순위 지정

--- src/main/java/io/security/basicsecurity2/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity2/SecurityConfig.java
@@ -1,12 +1,15 @@
 package io.security.basicsecurity2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/io/security/basicsecurity2/SecurityConfig2.java
+++ src/main/java/io/security/basicsecurity2/SecurityConfig2.java
@@ -1,10 +1,12 @@
 package io.security.basicsecurity2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Order(1)
 public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
