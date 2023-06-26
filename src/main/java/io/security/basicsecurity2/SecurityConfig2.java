07fb4f875678149f1cd5195465f61307aee2db5e
doyoun <doyoun8813@gmail.com>
2023-06-26 23:03:52 +0900

    다중 보안 설정 학습을 위한 시큐리티 보안 설정 클래스

--- src/main/java/io/security/basicsecurity2/SecurityConfig2.java
+++ src/main/java/io/security/basicsecurity2/SecurityConfig2.java
@@ -2,20 +2,16 @@ package io.security.basicsecurity2;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .antMatcher("/admin/**")
             .authorizeRequests()
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
             .and()
-            .httpBasic();
+            .formLogin();
     }
-
 }
