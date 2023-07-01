30d7dcf5032d987e1a712529a2860cd1afc9ccf2
doyoun <doyoun8813@gmail.com>
2023-07-02 00:27:02 +0900

    SecurityContext 객체 저장방식 변경 SecurityContext Thread 확인

--- src/main/java/io/security/basicsecurity2/SecurityConfig2.java
+++ src/main/java/io/security/basicsecurity2/SecurityConfig2.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @Order(1)
@@ -15,5 +16,6 @@ public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
             .anyRequest().permitAll()
             .and()
             .formLogin();
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
